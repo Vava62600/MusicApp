c63a4dcbc4be131f93a11bda301a3b7c1936f28e
Vava62600 <aideenligne62@gmail.com>
2024-08-31 12:33:57 +0200

    V0.1 for Linux ( Debian distros )

---
+++ platform/linux/pulse_pipe_wrapper.h
@@ -0,0 +1,17 @@
+#ifndef PULSE_PIPE_WRAPPER_H
+#define PULSE_PIPE_WRAPPER_H
+
+#include <string>
+#include <vector>
+
+class PulsePipeWrapper {
+public:
+    static void initializePulseAudio();
+    static void initializePipeWire();
+    static void setOutputDevice(const std::string& deviceName);
+    static void createVirtualOutput(const std::string& virtualOutputName);
+    static std::string getCurrentOutputDevice();
+    static void applyEffect(const std::string& effectName);
+};
+
+#endif // PULSE_PIPE_WRAPPER_H
